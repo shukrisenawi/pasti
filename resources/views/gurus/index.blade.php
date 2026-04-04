d1c20eedde8988df6d0a311bf957744f7e567f0e
ShukriSenawi <shukrisenawi@gmail.com>
2026-04-04 09:30:31 +0800

    Baiki tooltip avatar guru supaya stabil di mobile dan desktop

--- resources/views/gurus/index.blade.php
+++ resources/views/gurus/index.blade.php
@@ -45,8 +45,8 @@
                             <a href="{{ route('users.gurus.edit', $guru) }}" class="block" aria-label="Lihat profil {{ $guru->display_name }}">
                                 <x-avatar :guru="$guru" size="h-12 w-12" rounded="rounded-xl" border="border border-slate-200" />
                             </a>
-                            <div class="pointer-events-none absolute left-1/2 top-full z-30 mt-2 hidden -translate-x-1/2 rounded-xl border border-slate-200 bg-white p-1 shadow-xl group-hover:block">
-                                <img src="{{ $guru->avatar_url }}" alt="{{ $guru->display_name }}" class="h-[150px] w-[150px] rounded-lg object-cover">
+                            <div class="pointer-events-none absolute left-1/2 top-0 z-30 hidden -translate-x-1/2 -translate-y-[calc(100%+0.5rem)] md:block md:invisible md:opacity-0 md:transition md:duration-150 md:group-hover:visible md:group-hover:opacity-100">
+                                <div class="h-[150px] w-[150px] overflow-hidden rounded-xl border border-slate-200 bg-white p-1 shadow-xl"><img src="{{ $guru->avatar_url }}" alt="{{ $guru->display_name }}" class="h-full w-full rounded-lg object-cover"></div>
                             </div>
                         </div>
                         <div class="min-w-0">
@@ -84,6 +84,7 @@
 
     <div class="mt-4">{{ $gurus->links() }}</div>
 </x-app-layout>
+
 
 
 
