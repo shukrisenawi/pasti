6f513adcdc215d4fc68164a6c53f1be2fdc4bfbb
ShukriSenawi <shukrisenawi@gmail.com>
2026-04-05 10:12:05 +0800

    Naikkan had upload gambar PASTI kepada 7MB

--- app/Http/Controllers/PastiController.php
+++ app/Http/Controllers/PastiController.php
@@ -182,7 +182,7 @@ class PastiController extends Controller
             'phone' => ['nullable', 'string', 'max:30'],
             'manager_name' => ['nullable', 'string', 'max:255'],
             'manager_phone' => ['nullable', 'string', 'max:30'],
-            'image' => ['nullable', 'image', 'mimes:jpg,jpeg,png,webp', 'max:3072'],
+            'image' => ['nullable', 'image', 'mimes:jpg,jpeg,png,webp', 'max:7168'],
         ];
     }
 
@@ -193,7 +193,7 @@ class PastiController extends Controller
             'phone' => ['required', 'string', 'max:30'],
             'manager_name' => ['required', 'string', 'max:255'],
             'manager_phone' => ['required', 'string', 'max:30'],
-            'image' => ['nullable', 'image', 'mimes:jpg,jpeg,png,webp', 'max:3072'],
+            'image' => ['nullable', 'image', 'mimes:jpg,jpeg,png,webp', 'max:7168'],
         ];
     }
 
