851bad88eaac46895824238f0aef2fab0a918d59
shukri <shukrisenawi@gmail.com>
2026-04-23 10:28:13 +0800

    Buang alert kejayaan simpan data

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -558,10 +558,6 @@
                 </div>
             @endif
 
-            @if (session('status'))
-                <div class="alert alert-success">{{ session('status') }}</div>
-            @endif
-
             @if ($errors->any())
                 <div class="alert alert-error">
                     <div>
